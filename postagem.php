aea4d1abf7ac8ea889510f09f2ae254146b6f069
Ricardo Ferreira <ricardo.maurici@hotmail.com>
2014-08-08 19:53:23 -0300

    geracao do json com php

---
+++ postagem.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json');
+
+$pdo = new PDO("mysql:host=localhost; dbname=territorio", "root", 1234);
+
+$posts = [];
+
+$start = isset($_GET['start']) ? (int)$_GET['start'] - 1 : 0;
+$count = isset($_GET['count']) ? (int)$_GET['count'] : 1;
+
+$post = $pdo->query("SELECT SQL_CALC_FOUND_ROWS * FROM publicacao LIMIT {$start}, {$count}");
+
+$postsTotal = $pdo->query("SELECT FOUND_ROWS() AS count")->fetch(PDO::FETCH_ASSOC)['count'];
+
+if($postsCount = $post->rowCount()){
+	$posts = $post->fetchAll(PDO::FETCH_OBJ);
+}
+echo json_encode(array(
+	'items' => $posts,
+	'last' => ($start+$count) >= $postsTotal ? true : false,
+	'start' => $start,
+	'count' => $count
+));
+
+?>
